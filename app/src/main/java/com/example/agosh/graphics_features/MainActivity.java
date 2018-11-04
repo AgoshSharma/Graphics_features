fd00541868931cd17d71c7cfe59615e30b15a0ef
agosh <agosh.sharma.14@gmail.com>
2018-11-04 21:15:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/agosh/graphics_features/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.agosh.graphics_features;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
